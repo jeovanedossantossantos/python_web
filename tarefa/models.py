e19d347a51b26604b92b8c8e4ec941633a43d02c
jeovanedossantossantos <jeovane.ssantos@outlook.com>
2023-08-16 21:50:11 -0300

    chore: created models

--- tarefa/models.py
+++ tarefa/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from uuid import uuid4
+from user.models import UserModel
 # Create your models here.
+
+class TarefaModel(models.Model):
+
+    id=models.UUIDField(primary_key=True,default=uuid4,editable=False)
+    user = models.ForeignKey(UserModel,on_delete=models.CASCADE,related_name="tarefa")
+    nome=models.CharField(max_length=150)
+    decricao=models.CharField(max_length=254)
+    feito = models.BooleanField(default=False)
+    delete=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from uuid import uuid4
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class UserModel(AbstractUser):
+    TIPO_CHOICES =[
+        ("root","Root"),
+        ("client","Client"),
+    ]
+    id=models.UUIDField(primary_key=True,default=uuid4,editable=False)
+    suspenso=models.BooleanField(default=False)
+    email=models.EmailField(unique=True)
+    tipo=models.CharField(max_length=10,choices=TIPO_CHOICES,default="client")
